0551e23ac6bb179b0a228850527a993c6ca5bfd3
Gilberto Grimaldo <sisproit@gmail.com>
2026-07-26 08:43:38 -0500

    Agregar Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+import App from "./App";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <>
+      <App />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  </React.StrictMode>
+);
